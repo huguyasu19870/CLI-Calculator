2c3aa1cdb059121e6137fffb719af2af0466f595
huguyasu19870 <137283325+huguyasu19870@users.noreply.github.com>
2026-06-01 07:02:31 +0900

    Update operation.cpp

--- operation.cpp
+++ operation.cpp
@@ -1,16 +1,25 @@
 #include <stdexcept>
+#include <string>
+#include <vector>
+#include <climits>
+#include <iostream>
 #include "operation.h"
+#include "input_cleaner.h"
 
 int SimpleOperation::factorialOp(int a){
+    //std::cout << "[Debug] function factorialOp's variable a is: " << a << std::endl;
     bool isInputNegative = false;
     if(a == 0){
         return 1;
+        //If input of a factorial is zero, then return one.
+        //And if input of a factorial is negative, revert the sign to positive and note the operation.
     } else if(a < 0){
         a *= -1;
         isInputNegative = true;
     }
-    int result = 1;
-    for(int i = a; i > 0; i--){
+
+    int result = a;
+    for(int i = (a - 1); i > 0; i--){
         result *= i;
     }
     if(isInputNegative){
@@ -24,7 +33,7 @@ int SimpleOperation::exponentOp(int a, int b){
     }
     int result = a;
     if(b > 0){
-        for(int i = 0; i < b; i++){
+        for(int i = 0; i < b-1; i++){
             result *= a;
         }
     } else if(b < 0){
@@ -33,10 +42,14 @@ int SimpleOperation::exponentOp(int a, int b){
     return result;
 }
 
-int TypeConvert::letterToInt(std::string &input){
-    return std::stoi(input);
+int TypeConvert::letterToInt(const std::string &input){
+    if(input.length() == 1){
+        return TypeConvert::letterToInt(input[0]);
+    } else{
+        return std::stoi(input);
+    }
 }
-int TypeConvert::letterToInt(char &i){
+int TypeConvert::letterToInt(const char &i){
     int result;
     if(i == '0'){
         result = 0;
