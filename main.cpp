c9e27ea717541df16cbd611c89be4a9629d84aaf
huguyasu19870 <137283325+huguyasu19870@users.noreply.github.com>
2026-06-01 07:09:58 +0900

    Refactor main function and add test functionality

--- main.cpp
+++ main.cpp
@@ -4,164 +4,165 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <array>
+#include <algorithm>
 #include "main.h"
 #include "operation.h"
 #include "input_cleaner.h"
+#include "test_program.h"
+#include "calculation_Logic.h"
 
 int main()
 {
     inputOrganization inputOrganization;
     TypeConvert TypeConvert;
-    SimpleOperation SimpleOperation;
+    CalculationLogic CalculationLogic;
+    calcTest calcTest;
+
     std::string inputValue;
-emptyInput:
+    bool testMode = false;
+    bool shouldEnd = false;
+reInput:
     std::cout << "Please input the equation." << std::endl;
+    std::cout << "If you want to end this program, type \"Quit \" or \"Exit\"." << std::endl;
     std::cin >> inputValue;
     if(inputValue.empty()){
         std::cout << "Provided input was empty." << std::endl;
-        goto emptyInput;
+        goto reInput;
+    } else if((inputValue == "exit") || (inputValue == "Exit") || (inputValue == "quit") || (inputValue == "Quit")){
+        std::cout << "Exit command received.";
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        shouldEnd = true;
+        return 0;
+    } else if((inputValue == "test") || (inputValue == "Test")){
+        testMode = true;
+        goto testInit;
     }
     inputValue = inputOrganization.inputCleaner(inputValue);
     if(inputValue.empty()){
         std::cout << "Provided input resolved into empty." << std::endl;
-        goto emptyInput;
+        goto reInput;
     }
     inputValue = inputOrganization.inputCleaner(inputValue);
+    //Above codes essentially captures the programming into while loop if input is empty.
 
+
+    if(false){
+nullOutput:
+            std::cout << "Variable testPhase pointed outside of array range." << std::endl;
+            return 1;
+        }
+testInit:
+    if(testMode){
+        int testOp = calcTest.getTestOps();
+        int testPhase = calcTest.getTestNum();
+        int testType = calcTest.getTestType();
+        std::array<std::string, 5> testArray;
+        if(testType == 0){
+            testArray = calcTest.simpleIntegerOperationTestCases(testOp);
+        }
+        inputValue = testArray[testPhase];
+    }
     std::unordered_map<unsigned long long int, std::string> calcFlg = inputOrganization.calculationSpecifier(inputValue);
-    bool isDoneProcessing = false;
-    unsigned long long int calcStep = 0;
+//    std::unordered_map<unsigned long long int, unsigned long long int> location = inputOrganization.getOopLocation();
+//    unsigned long long int delCount = 0;
+    std::vector<std::string> calcFlg2 = inputOrganization.returnOrderedFormula(calcFlg);
+    for(int i = 0; i < calcFlg2.size(); i++){
+        std::string elem = calcFlg2[i];
+        //std::cout << "[Debug] function main's elements of calcFlg2 is: " << elem << std::endl;
+        if((elem == "<") || (elem == ">") || (elem == "=") || (elem == "~=") || (elem == ">=") || (elem == "<=")){
+            inputOrganization.setComparisonLocation(i);
+        }
+    }
     int result1;
     int result2;
     int result;
-    int calc1;
-    int calc2;
-    int calcTemp;
-    std::string currentCalc;
-    bool isFirstHalf;
-//    std::unordered_map<unsigned long long int, unsigned long long int> location = inputOrganization.getOopLocation();
-    unsigned long long int compLoc = inputOrganization.getComparisonLocation();
-//    unsigned long long int delCount = 0;
-    std::vector<std::string> calcFlg2;
-    calcFlg2.reserve(calcFlg.size());
-    for(unsigned long long int i = 0; i < calcFlg.size(); i++){
-        calcFlg2.push_back(calcFlg.at(i));
-        if(i == ULLONG_MAX){
-            break;
-        }
+    calcFlg2 = CalculationLogic.calcLogic(calcFlg2);
+    if(calcFlg2[0] == "null"){
+        goto nullOutput;
     }
-    int calcOps = 0;
-    /*
-    0 = Factorial
-    1 = Exponent
-    2 = Multiplication/Division
-    3 = Addition/Subtraction
-    4 = Comparison ; for now, maybe sent to -1, or maybe not need one to represent it.
-    */
-    while(!isDoneProcessing){
-        //I have tried to do smart thing and minimize the amount of calculation by doing some circus routine.
-        //I was not that smart or knowledgeable, so I am brute-forcing the result out of this.
-        isFirstHalf = calcStep < compLoc;
-        currentCalc = calcFlg2[calcStep];
-        /*
-        try{
-            calcPhase = location.at(calcStep);
-            currentCalc = calcFlg.at(calcPhase);
-        } catch (std::out_of_range){
-            std::cout << "Attempted access to out of range unordered map element." << std::endl;
-            std::cout << "Please contact the nearest software developer." << std::endl;
-        }
-        */
-        if((currentCalc == "!") && (calcOps == 0)){
-            calcTemp = SimpleOperation.factorialOp(TypeConvert.letterToInt(calcFlg2[calcStep-1]));
-            calcFlg2[calcStep-1] = std::to_string(calcTemp);
-            calcFlg2.erase(calcFlg2.begin() + calcStep);
-            calcStep -= 1;
-        } else if((currentCalc == "^") && (calcOps == 1)){
-            calc1 = TypeConvert.letterToInt(calcFlg2[calcStep-1]);
-            calc2 = TypeConvert.letterToInt(calcFlg2[calcStep+1]);
-            calcTemp = SimpleOperation.exponentOp(calc1, calc2);
-            calcFlg2[calcStep-1] = std::to_string(calcTemp);
-            calcFlg2.erase(calcFlg2.begin() + calcStep, calcFlg2.begin() + calcStep+1);
-            calcStep -= 1;
-        } else if((calcOps == 2) && ((currentCalc == "*") || (currentCalc == "/"))){
-            calc1 = TypeConvert.letterToInt(calcFlg2[calcStep-1]);
-            calc2 = TypeConvert.letterToInt(calcFlg2[calcStep+1]);
-            if(currentCalc == "*"){
-                calcTemp = calc1 * calc2;
-            } else if(currentCalc == "/"){
-                calcTemp = calc1 / calc2;
-            }
-            calcFlg2[calcStep-1] = std::to_string(calcTemp);
-            calcFlg2.erase(calcFlg2.begin() + calcStep, calcFlg2.begin() + calcStep+1);
-            calcStep -= 1;
-        } else if((calcOps == 3) && ((currentCalc == "+") || (currentCalc == "-"))){
-            calc1 = TypeConvert.letterToInt(calcFlg2[calcStep-1]);
-            calc2 = TypeConvert.letterToInt(calcFlg2[calcStep+1]);
-            if(currentCalc == "+"){
-                calcTemp = calc1 + calc2;
-            } else if(currentCalc == "-"){
-                calcTemp = calc1 - calc2;
-            }
-            calcFlg2[calcStep-1] = std::to_string(calcTemp);
-            calcFlg2.erase(calcFlg2.begin() + calcStep, calcFlg2.begin() + calcStep+1);
-            calcStep -= 1;
-        }
-        if(isFirstHalf && (calcStep == compLoc) && (calcOps < 3)){
-            calcStep = 0;
-            calcOps++;
-        } else if(isFirstHalf && (calcStep == compLoc) && (calcOps == 3)){
-            isFirstHalf = false;
-            calcOps = 0;
-        } else if((!isFirstHalf) && (calcStep == (calcFlg2.size()-1)) && (calcOps < 3)){
-            calcStep = compLoc;
-            calcOps++;
-        } else if((!isFirstHalf) && (calcStep == (calcFlg2.size()-1)) && (calcOps == 3)){
-            isDoneProcessing = true;
-        }
-        if((isDoneProcessing == true) && (compLoc != 0)){
-            calcOps = 4;
-        }
-        if(calcStep < ULLONG_MAX){
-            calcStep++;
+    int calcOps = CalculationLogic.getCalcOps();
+    //std::cout << "[Debug] main function's variable calcOps is: " << calcOps << std::endl;
+
+    if((calcOps == 4) && (!testMode)){
+        std::vector<std::string> comparisonResultVector = CalculationLogic.singleComparisonLogic(calcFlg2);
+        std::string compOp = comparisonResultVector[1];
+        result1 = TypeConvert.letterToInt(comparisonResultVector[0]);
+        result2 = TypeConvert.letterToInt(comparisonResultVector[2]);
+        bool compResult = static_cast<bool>(TypeConvert.letterToInt(comparisonResultVector[3]));
+        std::string stringCompResult;
+        if(compResult){
+            stringCompResult = "true";
         } else {
-            break;
+            stringCompResult = "false";
         }
-    }
-    if(calcOps == 4){
-        std::string compOp = calcFlg2[1];
-        result1 = TypeConvert.letterToInt(calcFlg2[0]);
-        result2 = TypeConvert.letterToInt(calcFlg2[2]);
-        bool compResult;
-        if(compOp == "<"){
-            compResult = result1 < result2;
-        } else if((compOp == "<=")){
-            compResult = result1 <= result2;
-        } else if(compOp == ">"){
-            compResult = result1 > result2;
-        } else if((compOp == ">=")){
-            compResult = result1 >= result2;
-        } else if(compOp == "="){
-            compResult = result1 == result2;
-        } else if(compOp == "!="){
-            compResult = result1 != result2;
-        } else if(compOp == "~="){
-            int digits1 = calcFlg2[0].length();
-            int digits2 = calcFlg2[2].length();
-            int roudingDigit = digits2-1;
-            if(roudingDigit > 0){
-                compResult = ((digits2+(roudingDigit*5)) > digits1) && ((digits2-(roudingDigit*5)) < digits1);
-            } else if(roudingDigit == 0){
-                compResult = ((digits2+(0.5f)) > digits1) && ((digits2-(0.5f)) < digits1);
-            }
-        }
-        std::cout << "Result of comparison" << inputValue << " is simplified to" << std::endl;
+        std::cout << "Result of comparison " << inputValue << " is simplified to" << std::endl;
         std::cout << result1 << compOp << result2 << std::endl;
-        std::cout << "Which is " << compResult << std::endl;
-    } else {
+        std::cout << "Which is " << stringCompResult << std::endl << std::endl;
+    } else if(!testMode) {
         result = TypeConvert.letterToInt(calcFlg2[0]);
-        std::cout << "Result of " << inputValue << " is" << std::endl << result << std::endl;
+        std::cout << "Result of " << inputValue << " is" << std::endl << result << std::endl << std::endl;
+    } else {
+        int testOp = calcTest.getTestOps();
+        int testPhase = calcTest.getTestNum();
+        int testType = calcTest.getTestType();
+        std::string testResult = calcFlg2[0];
+        if(testType == 0){
+            std::array<int, 5> answerArray = calcTest.simpleIntegerOperationTestAnswer(testOp);
+            int expTestResult = answerArray[testPhase];
+            int actualTestResult;
+            if(testOp < 6){
+                actualTestResult = TypeConvert.letterToInt(calcFlg2[0]);
+            } else if(testOp >= 6){
+                std::vector<std::string> comparisonFullResult = CalculationLogic.singleComparisonLogic(calcFlg2);
+                actualTestResult = TypeConvert.letterToInt(comparisonFullResult[3]);
+            }
+            bool isResultEqual = expTestResult == actualTestResult;
+            calcTest.insertSimpleResultTempArray(isResultEqual, testPhase);
+            calcTest.incrementTestNum();
+            testPhase++;
+            if(testPhase == answerArray.size()){
+                std::array<bool, 5> resultArray = calcTest.getSimpleResultTempArray();
+                bool hasNegative = false;
+                for(const bool& elem: resultArray){
+                    if(!elem){
+                        hasNegative = true;
+                        break;
+                    }
+                }
+                if(hasNegative){
+                    calcTest.insertSimpleResultArray(false, testOp);
+                } else {
+                    calcTest.insertSimpleResultArray(true, testOp);
+                }
+                calcTest.setTestNum(0);
+                calcTest.incrementTestOps();
+                testOp++;
+            }
+            std::string comparisonResultString;
+            if(isResultEqual){
+                comparisonResultString = "true";
+            } else {
+                comparisonResultString = "false";
+            }
+            std::cout << "Test question is " << inputValue << "." << std::endl;
+            std::cout << "Test question resolved to " << actualTestResult << "." << std::endl;
+            std::cout << "Expected result is " << expTestResult << "." << std::endl;
+            std::cout << "Which is " << comparisonResultString << "." << std::endl;
+            std::cout << std::endl;
+            if(testOp == 8){
+                goto endTest;
+            }
+
+        }
+        goto testInit;
     }
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    if(!shouldEnd){
+        goto reInput;
+    }
+    if(testMode){
+endTest:
+        calcTest.finalTestValidation();
+    }
     return 0;
 }
